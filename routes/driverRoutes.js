b4ae79c11368411b2110c95d4e95894b4125dae5
rselimsahin <selimsahin0797@gmail.com>
2019-02-27 14:54:56 +0300

    Simplified routes

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -2,11 +2,13 @@
 module.exports=function(app){
     var driver=require('../controllers/driverController.js');
 
-    app.route('/drivers')
-        .get(driver.list_drivers)
+    app.route('/create_driver')
         .post(driver.create_driver);
     
-    app.route('/drivers/:driverId')
+    app.route('/list_drivers')
+        .get(driver.list_drivers)
+
+    app.route('/driver_operations/:driverId')
         .get(driver.read_driver)
         .put(driver.update_driver)
         .delete(driver.delete_driver);
